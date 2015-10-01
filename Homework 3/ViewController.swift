d8a6f6a2f442c37afb1594ca5a16efd47f8ae68d
Rob Hunsaker <hunsaker.rob@gmail.com>
2015-10-01 13:55:10 -0700

    finished homework 3

--- Homework 3/ViewController.swift
+++ Homework 3/ViewController.swift
@@ -8,18 +8,84 @@
 
 import UIKit
 
-class ViewController: UIViewController {
 
+
+
+
+class ViewController: UIViewController {
+  
+  
+  var counterOne = 0
+  var counterTwo = 0
+  let arrayOne = [0, 1, 2, 3, 4, 5]
+  let arrayTwo = [100, 101, 102, 103, 104, 105]
+  
+  @IBOutlet weak var labelOne: UILabel!
+  
+  @IBOutlet weak var labelTwo: UILabel!
+  
+  
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view, typically from a nib.
+    labelOne.text = "\(arrayOne[0])"
+    labelTwo.text = "\(arrayTwo[0])"
   }
-
-  override func didReceiveMemoryWarning() {
-    super.didReceiveMemoryWarning()
-    // Dispose of any resources that can be recreated.
+  
+  @IBAction func buttonPressed(sender: AnyObject) {
+    counterOne++
+    
+    if counterOne >= arrayOne.count {
+      counterOne = 0
+      
+    }
+    labelOne.text = "\(arrayOne[counterOne])"
   }
+  @IBAction func buttonPressedTwo(sender: AnyObject) {
+    counterTwo++
+    if counterTwo >= arrayTwo.count {
+      counterTwo = 0
+      
+    }
+    
+    labelTwo.text = "\(arrayTwo[counterTwo])"
+    
+  }
+  
+  
 
-
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
 }
 
